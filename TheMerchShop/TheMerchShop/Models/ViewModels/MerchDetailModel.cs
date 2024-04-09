1f9749cea40e93088b9226836579f9c891a5fd7c
James Vernon <Vernon.James@SPCollege.edu>
2024-04-09 17:45:39 -0400

    update

---
+++ TheMerchShop/TheMerchShop/Models/ViewModels/MerchDetailModel.cs
@@ -0,0 +1,21 @@
+﻿namespace TheMerchShop.Models
+{
+    public class MerchDetailModel
+    {
+        public Merch Merch { get; set; } 
+        public List<Vendor> Vendors { get; set; } = new List<Vendor>();
+
+        public List<string> VendorNames()
+        {
+
+           List<string> names = new List<string>();
+
+           foreach (Vendor vendor in Vendors)
+            {
+                names.Add(vendor.Name);
+            }
+
+           return names;
+        }
+    }
+}
